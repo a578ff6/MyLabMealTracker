4374110f0f335ca2b998042844121db27acc977a
wayne tsao <a578ff6@gmail.com>
2023-10-08 17:33:13 +0800

    Initial Commit

---
+++ MyLabMealTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLabMealTracker
+//
+//  Created by 曹家瑋 on 2023/10/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
